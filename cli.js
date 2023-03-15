e14cc22d3e16540981bc3f4de4ca3f5297061ecc
Ashams2000 <alishamsol2000@gmail.com>
2023-03-14 21:43:01 -0400

    Modified the cli.js to work properly

--- cli.js
+++ cli.js
@@ -1,75 +1,82 @@
 #!/user/bin/env node
 
-import moment from "moment-timezone";
-import minimist from "minimist";
-import fetch from "node-fetch";
+import moment from 'moment-timezone';
+import minimist from 'minimist';
+import fetch from 'node-fetch';
 
 const args = minimist(process.argv.slice(2));
-let text = 'Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE' +
-    '-h            Show this help message and exit.' +
-    '-n, -s        Latitude: N positive; S negative.' +
-    '-e, -w        Longitude: E positive; W negative.' +
-    '-z            Time zone: uses tz.guess() from moment-timezone by default.' +
-    '-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.' +
-    '-j            Echo pretty JSON from open-meteo API and exit.';
+const days = args.d
+const timezone = setTimeZone();
+const latitude = setLatitude();
+const longitude = setLongitude();
 
-
-if(args.h){
-	try{
-		console.log(text);
-		process.exit(0);
-	}
-	catch(err){
-		process.exit(1);
-	}
+if (args.h){
+	// Display the message for help 
+	console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n',
+       '    -h            Show this help message and exit.\n',
+       '    -n, -s        Latitude: N positive; S negative.\n',
+       '    -e, -w        Longitude: E positive; W negative.\n',
+       '    -z            Time zone: uses tz.guess() from moment-timezone by default.\n',
+       '    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n',
+       '    -j            Echo pretty JSON from open-meteo API and exit.\n')
+       process.exit(0)
 }
 
-// Set the timezone
-var timezone = moment.tz.guess(); 
 
-if(args.z){
-    timezone = args.z
-} else {
-    timezone = moment.tz.guess();
+// Initialize the parameters to be used in the API URL
+
+function setTimeZone() {
+    if (args.z != null) {
+        return args.z
+    } else {
+        return moment.tz.guess();
+    }
 }
-
-// Set longitude and latitude
-var latitude = args.n || (args.s * -1.0);
-var longitude = args.e || (args.w * -1.0);
+function setLatitude() {
+    let lat
+    if (args.n != null) {
+        lat = args.n
+    } else {
+        lat = args.z * -1
+    }
+    lat = lat.toFixed(2)
+    return lat
+}
+function setLongitude() {
+    let long
+    if (args.e != null) {
+        long = args.e
+    } else {
+        long = args.w * -1
+    }
+    long = long.toFixed(2)
+    return long
+}
 
 const response = await fetch(
   `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&current_weather=true&timezone=${timezone}`
 );
-
-// Get the data
 const data = await response.json();
 
-if(args.j){
-    console.log(data);
-    process.exit(0);
+if (args.j) {
+    console.log(data)
+    process.exit(0)
 }
 
-const days = input.d;
-if (days == 0) {
-	console.log("today.");
-	if(data.daily.precipitation_hours[0] > 0) {
-	 	console.log("You might need your galoshes");
-	}  else{
-	console.log("You will not need your galoshes");
-	}
-}
-else if (days > 1) {
-	console.log('in ${days} days.');
-	if( data.daily.precipitation_hours[days] > 0) {
-		console.log("You might need your galoshes");
-	} else{
-	console.log("You will not need your galoshes")
-	}
+// Find the exact hours that it will rain
+const precip = data.daily.precipitation_hours[days]
+
+if (precip != 0) {
+    process.stdout.write("You might need your galoshes ")
 } else {
-	console.log("tomorrow.");
-	if(data.daily.precipitation_hours[1] > 0) {
-		console.log("You might need your galoshes");
-	} else{
-		console.log("You will not need your galoshes");
+    process.stdout.write("You will not need your galoshes ")
+}
 
-			
+if (days == 0) {
+    process.stdout.write("today.")
+  } else if (days > 1) {
+    process.stdout.write("in " + days + " days.")
+  } else {
+    process.stdout.write("tomorrow.")
+  }
+
